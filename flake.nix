fd5985dc53b3f5afb4644263659bc3f2f0dfe332
Reyu Zenfold <reyu@reyuzenfold.com>
2021-06-05 17:09:44 -0400

    Add Beam dependencies

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   description = "Home Inventory Management";
-
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
@@ -21,14 +20,37 @@
             name = "Inventory";
             root = ./.;
             withHoogle = true;
-            overrides = self: super: with pkgs.haskell.lib; {
-              # Use callCabal2nix to override Haskell dependencies here
-              # cf. https://tek.brick.do/K3VXJd8mEKO7
-            };
+            overrides =
+              let
+                haskell-beam = pkgs.fetchFromGitHub {
+                  owner = "haskell-beam";
+                  repo = "beam";
+                  rev = "f461925cf94a096238b8c37f30318c46d1e9f9cf";
+                  sha256 = "WFegq92+IuNK3uFguEfkVNIkBD0s0VMhWrYUeWiN04U=";
+                  fetchSubmodules = true;
+                };
+                beam = self: subpkg: self.callCabal2nix "beam-${subpkg}" (haskell-beam + "/beam-${subpkg}") { };
+              in
+              self: super: with pkgs.haskell.lib; {
+                # Use callCabal2nix to override Haskell dependencies here
+                # cf. https://tek.brick.do/K3VXJd8mEKO7
+                beam-core =
+                  pkgs.haskell.lib.dontCheck (
+                    self.callCabal2nix "beam-core" (haskell-beam + "/beam-core") { });
+                beam-postgres =
+                  pkgs.haskell.lib.dontCheck (
+                    self.callCabal2nix "beam-postgres" (haskell-beam + "/beam-postgres") { });
+                beam-migrate =
+                  pkgs.haskell.lib.dontCheck (
+                    self.callCabal2nix "beam-migrate" (haskell-beam + "/beam-migrate") { });
+                beam-migrate-cli =
+                  pkgs.haskell.lib.dontCheck (
+                    self.callCabal2nix "beam-migrate-cli" (haskell-beam + "/beam-migrate-cli") { });
+              };
             modifier = drv:
               pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
               [
-                # Specify your build/dev dependencies here. 
+                # Specify build/dev dependencies here.
                 cabal-fmt
                 cabal-install
                 ghcid
