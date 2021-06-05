a87a5d6ed28b2fde9d437d538bc0b810d8c2c6b2
Reyu Zenfold <reyu@reyuzenfold.com>
2021-06-05 13:16:59 -0400

    Base Layout Completed

--- flake.nix
+++ flake.nix
@@ -2,37 +2,46 @@
   description = "Home Inventory Management";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        haskellPackages = pkgs.haskellPackages;
-
-        jailbreakUnbreak = pkg:
-          pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
-
-        packageName = "Inventory";
-      in {
-        packages.${packageName} = # (ref:haskell-package-def)
-          haskellPackages.callCabal2nix packageName self rec {
-            # Dependency overrides go here
+        overlays = [ ];
+        pkgs =
+          import nixpkgs { inherit system overlays; config.allowBroken = false; };
+        project = returnShellEnv:
+          pkgs.haskellPackages.developPackage {
+            inherit returnShellEnv;
+            name = "Inventory";
+            root = ./.;
+            withHoogle = true;
+            overrides = self: super: with pkgs.haskell.lib; {
+              # Use callCabal2nix to override Haskell dependencies here
+              # cf. https://tek.brick.do/K3VXJd8mEKO7
+            };
+            modifier = drv:
+              pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+              [
+                # Specify your build/dev dependencies here. 
+                cabal-fmt
+                cabal-install
+                ghcid
+                haskell-language-server
+                pkgs.nixpkgs-fmt
+              ]);
           };
+      in
+      {
+        # Used by `nix build` & `nix run` (prod exe)
+        defaultPackage = project false;
 
-        defaultPackage = self.packages.${system}.${packageName};
-
-        devShell = pkgs.mkShell {
-          buildInputs = with haskellPackages; [
-            haskell-language-server
-            ghcid
-            cabal-install
-            hpack
-          ];
-          inputsFrom = builtins.attrValues self.packages.${system};
-        };
+        # Used by `nix develop` (dev shell)
+        devShell = project true;
       });
 }
